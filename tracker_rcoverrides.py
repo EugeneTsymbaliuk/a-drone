3608cdfdc1c11cd27751e031054e50e23a093f0a
Eugene Tsymbaliuk <31383416+EugeneTsymbaliuk@users.noreply.github.com>
2024-12-12 09:47:16 +0200

    Add files via upload

--- tracker_rcoverrides.py
+++ tracker_rcoverrides.py
@@ -15,16 +15,16 @@ roi_size = 150
 # Center of video window
 x = dispW // 2
 y = dispH // 2
+fps=0
 
 # Wait 60 seconds
 print("Wait 60 seconds")
-#sleep(60)
+sleep(60)
 
 # Create the connection to drone
 print('Connecting to FC')
-#vehicle = connect('tcp:192.168.1.140:5762')
-#vehicle = connect('tcp:194.247.173.68:5773')
-vehicle = connect("/dev/ttyAMA0", baud=115200, wait_ready=True,  timeout=100, rate=15)
+#vehicle = connect('tcp:192.168.1.145:5762', rate=40)
+vehicle = connect("/dev/ttyAMA0", baud=57600, wait_ready=True,  timeout=100, rate=40)
 print('Connected to FC')
 
 # Create picamera instance
@@ -41,47 +41,16 @@ picam2.start()
 # Object tracker
 tracker = cv.TrackerCSRT_create() # Initialize tracker with CSRT algorithm
 
-BB = None # Bounding Box
-#thr = 1600
-#thr_down = 1400
-#thr_up = 1800
-#ch3 = 0
-roll = 1500
+# Bounding Box
+BB = None
+
+# Channels1-4 values
+thr = 1500
 yaw = 1500
-#roll_right = 1600
-#roll_left = 1400
-#yaw_right = 1520
-#yaw_left = 1480
-
-'''
-def arm():
-    while not vehicle.is_armable:
-        print(" Waiting for vehicle to initialise...")
-        sleep(1)
-    print("Arming motors")
-    vehicle.armed = True
-    while not vehicle.armed:
-        print(" Waiting for arming...")
-        vehicle.armed = True
-        sleep(1)
-    vehicle.mode = VehicleMode("GUIDED")
-
-def takeOff(high):
-    print("Taking off on 20 meters")
-    msg = vehicle.message_factory.command_long_encode(0, 0, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 
-                                                      0, 0, 0, 0, 0, 0, 0, high)
-    # send command to vehicle
-    vehicle.send_mavlink(msg)
-
-def changAlt(alt):
-    msg = vehicle.message_factory.command_long_encode(0, 0, mavutil.mavlink.MAV_CMD_DO_CHANGE_ALTITUDE, 
-                                                      alt, MAV_FRAME_BODY_OFFSET_NED, 0, 0, 0, 0, 0)
-    # send command to vehicle
-    vehicle.send_mavlink(msg)
-'''
+roll = 1500
+pitch = 1200
 
 def rcOverrides(roll, pitch, thr, yaw):
-#    print(roll, pitch, thr, yaw)
     vehicle.channels.overrides = {'1': roll, '2': pitch, '3': thr, '4': yaw}
 
 def merge_roi(frame, roi, x, y):
@@ -89,48 +58,41 @@ def merge_roi(frame, roi, x, y):
     return frame
 
 def trackTarget(frame):
-#    global thr
     (success, box) = tracker.update(frame)
     if success:
         (x, y, w, h) = [int(v) for v in box]
         cv.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
-        yaw_error = (x + w/2) - dispW/2
+        roll_error = (x + w/2) - dispW/2
         pitch_error = (y + h/2) - dispH/2
-        #print(yaw_error)
-        if yaw_error > 10:
-            print("Yaw right")     
-            ch1 = roll + 100
-            ch4 = yaw + 10
-        elif yaw_error < -10:
-            print("Yaw left")     
-            ch1 = roll - 100
-            ch4 = yaw - 10
-#        elif pitch_error > 10:
-#            print("Throttle down")
-#            if thr < 1300:
-#                thr = 1350
-#            ch1 = roll
-#            thr = thr - 3
-#            ch4 = yaw
-#        elif pitch_error < -10:
-#            print("Throttle up")
-#            if thr > 1900:
-#                thr = 1850
-#            ch1 = roll
-#            ch3 = thr + 100
-#            ch4 = yaw
-        else:
-            print("Fly Forward")
-#            thr = start_thr + 10
-            ch1, ch4 = (roll, yaw)
-#        print(ch1, pitch, ch3, ch4)
-        rcOverrides(ch1, pitch, thr, ch4)
-    return success, frame
-#arm()
-#takeOff(20)
-#sleep(15)
-#flyBodyoffset(1000, 0, -10)
+        if roll_error > 20 and -20 < pitch_error < 20:
+            print("Right")
+            rcOverrides(1600, pitch, thr, 1550)
+        if roll_error < -20 and -20 < pitch_error < 20:
+            print("Left")
+            rcOverrides(1400, pitch, thr, 1450)
+        if pitch_error > 20 and -20 < roll_error < 20:
+            print("Down")
+            rcOverrides(roll, pitch, 1300, yaw)
+        if pitch_error < -5 and -20 < roll_error < 20:
+            print("Up")
+            rcOverrides(roll, pitch, 1700, yaw)
+        if roll_error > 20 and pitch_error > 20:
+            print("Right and Down")
+            rcOverrides(1600, pitch, 1300, 1550)
+        if roll_error > 20 and pitch_error < -5:
+            print("Right and Up")
+            rcOverrides(1600, pitch, 1700, 1550)
+        if roll_error < -20 and pitch_error < -5:
+            print("Left and Up")
+            rcOverrides(1400, pitch, 1700, 1450)
+        if pitch_error > 20 and roll_error < -20:
+            print("Left and Down")
+            rcOverrides(1400, pitch, 1300, 1450)
+        if -20 < roll_error < 20 and -5 < pitch_error < 20:
+            print("Fly forward")
+            rcOverrides(roll, pitch, thr, yaw)
 
+    return success, frame
 
 while True:
     tStart = time()
@@ -151,17 +113,15 @@ while True:
     
     if BB is not None:
         cv.putText(frame, "Tracking Enabled", (5,50), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 2)
-        cv.putText(frame, "Throttle: " + str(thr), (5,70), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 2)
+#        cv.putText(frame, "Throttle: " + str(thr), (5,70), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 2)
         success, frame = trackTarget(frame) # Track object
         
 
     cv.putText(frame, "Connected", (5,30), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 2)
+    cv.putText(frame, str(int(fps))+' FPS', (5,70), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,255), 2)
     cv.imshow("Frame", frame)
     key = cv.waitKey(1) & 0xFF
     
-#    chan1, chan2, chan3, chan4 = (vehicle.channels['9'], vehicle.channels['10'], vehicle.channels['11'], vehicle.channels['12'])
-#    rcOverrides(chan1, chan2, chan3, chan4)
-
     # Select Region on Interest (ROI) to track
 #    if key == ord("c"):
     if vehicle.channels['5'] > 1550 and BB is None:
@@ -171,15 +131,12 @@ while True:
         BB = (335, 235, 50, 50)        
 
         # Enable stabilize mode
-        if vehicle.mode != "STABILIZE":
-#            print("Stabilize mode enab12led")
-            vehicle.mode = VehicleMode("STABILIZE")
+        if vehicle.mode != "ALT_HOLD":
+            vehicle.mode = VehicleMode("ALT_HOLD")
 
         # Save throttle and pitch values
-        pitch = vehicle.channels['10']
-        thr = vehicle.channels['11']
-#        pitch = 1300
-#        thr = 1600     
+#        pitch = vehicle.channels['10']
+#        thr = vehicle.channels['11']
         # Run tracking
         tracker.init(frame, BB)
 
@@ -187,16 +144,20 @@ while True:
 #    if key == ord("v"):
     if vehicle.channels['5'] < 1550:
         BB = None
-#        print(vehicle.channels['9'], vehicle.channels['10'], vehicle.channels['11'], vehicle.channels['12'])
+#        print(vehicle.channels['5'], vehicle.channels['9'], vehicle.channels['10'], vehicle.channels['11'], vehicle.channels['12'])
         rcOverrides(vehicle.channels['9'], vehicle.channels['10'], vehicle.channels['11'], vehicle.channels['12'])
+
     # Close video window
     if key == ord("q"):
             # Close vehicle object
             vehicle.close()
             break
+
+    # FPS count
     tEnd=time()
     loopTime=tEnd-tStart
-    print(loopTime)
+#    print(loopTime)
+    fps=.9*fps + .1*(1/loopTime)
 
 # Stop tracking
 cv.destroyAllWindows()
